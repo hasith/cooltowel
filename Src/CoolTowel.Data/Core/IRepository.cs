a49224d5f948e21e9a80b301620c124d2d7e19c0
hasith <hasith@gmail.com>
2013-07-19 21:20:14 +0530

    adding entity framework support

---
+++ Src/CoolTowel.Data/Core/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolTowel.Data.Core
+{
+    public interface IRepository<T> where T: class
+    {
+        IQueryable<T> GetAll(string[] includes = null);
+
+        T GetById(int id);
+
+        void InsertOrUpdate(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+    }
+}
+
---
+++ Src/CoolTowel.Data/Core/Repository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+namespace CoolTowel.Data.Core
+{
+ 
+
+    public class Repository<T> : IRepository<T> where T : class, IIdentifier
+    {
+
+        protected DbContext DbContext { get; set; }
+        internal DbSet<T> DbSet { get; set; }
+
+        public Repository(DbContext context)
+        {
+            DbContext = context;
+            DbSet = context.Set<T>();
+        }
+
+
+        public virtual IQueryable<T> GetAll(string[] includes = null)
+        {
+            return DbSet.AsQueryable();
+        }
+
+
+        public T GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual void InsertOrUpdate(T entity)
+        {
+            DbSet.Add(entity);
+
+            if (entity.Id == 0)
+            {
+                DbSet.Add(entity);
+            }
+            else
+            {
+                DbSet.Attach(entity);
+                DbContext.Entry(entity).State = EntityState.Modified;
+            }
+        }
+
+        public virtual void Delete(T entityToDelete)
+        {
+            if (DbContext.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+            DbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(T entityToUpdate)
+        {
+            DbSet.Attach(entityToUpdate);
+            DbContext.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+
+}
